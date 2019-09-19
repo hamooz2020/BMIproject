edcbcf36b040732d2cac18bfa42eb5e17b5a359c
Hamza <hamooz7670@gmail.com>
2019-09-19 09:21:00 -0400

    Added my first question and initialized code

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,50 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        //declare
+        Scanner keyboard;
+        String heightFeet;
+
+        //initialize
+       keyboard = new Scanner(System.in);
+
+
+
+        System.out.println("");
+
+
+        System.out.println("What is your height \n(feet only)");
+        heightFeet = keyboard.nextLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
