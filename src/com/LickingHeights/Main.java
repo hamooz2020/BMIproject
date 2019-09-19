f190b9ce81beeb673aee694dd116eb1d4d0506f6
Hamza <hamooz7670@gmail.com>
2019-09-19 10:15:30 -0400

    Did BMI equation in metric

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -9,32 +9,44 @@ public class Main {
 
         //declare
         Scanner keyboard;
-        String heightFeet;
-        String heightInches;
+
+        double heightFeet,heightInches,weight,totalHeight,kg,meters;
 
 
 
         //initialize
        keyboard = new Scanner(System.in);
 
-
+        double FEETINCHES = 12;
 
         System.out.println("");
 
 
-        System.out.println("What is your height \n(feet only)");
-        heightFeet = keyboard.nextLine();
+        System.out.println("What is your height \n(feet only (x'11))");
+        heightFeet = keyboard.nextDouble();
 
-        System.out.println("What is your height \n(inches only)");
-        heightInches = keyboard.nextLine();
+        System.out.println("What is your height \n(inches only (6'x))");
+        heightInches = keyboard.nextDouble();
 
+        System.out.println("What is your weight in pounds?");
+        weight = keyboard.nextDouble();
 
+        totalHeight = heightFeet*12+heightInches;
 
+        System.out.println("Your height in Inches is...");
+        System.out.println(heightFeet*FEETINCHES+heightInches);
 
+        System.out.println("And in meters it is...");
+        System.out.println(totalHeight*0.0254);
 
+        System.out.println("Your weight in kilograms is...");
+        System.out.println(weight*0.453592);
 
+        kg = weight*0.45359;
+        meters = totalHeight*0.0254;
 
-
+        System.out.println("YOUR BMI IS...");
+        System.out.println(kg/meters);
 
 
 
